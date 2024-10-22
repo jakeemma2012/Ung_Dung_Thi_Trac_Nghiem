deb18b92b0c8a55f67f7440668c14e480042f219
jakeemma2012 <tranhuuphuc.k20.cva@gmail.com>
2024-10-22 21:08:45 +0700

    Create Project
    Nothing Yet

---
+++ App_Thi_Trac_Nghiem/App_Thi_Trac_Nghiem/Program.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using DevExpress.UserSkins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace App_Thi_Trac_Nghiem
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
